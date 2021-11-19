324fc80cf99d34a2528930eec998b99d6a6df16c
yipdamon <yipdamon@hotmail.com>
2021-11-18 19:26:16 -0500

    Added search feature, order, and login/signup page

--- src/components/PetItem/index.jsx
+++ src/components/PetItem/index.jsx
@@ -1,10 +1,29 @@
 import './styles.css';
 import { Button } from '../Button';
 import { Link } from 'react-router-dom';
+import PetsOrderContext from '../../context/petsOrderContext';
+import { useContext } from 'react';
 
 export const PetItem = (props) => {
 
     const {image, age, name, breed, type, id } = props;
+
+    const globalState = useContext(PetsOrderContext);
+
+    const addPetToCart = () => {
+
+        const pet = {
+            id,
+            name,
+            image,
+            breed,
+            type,
+            age
+        }
+        globalState.addPetToOrder(pet);
+        console.log(globalState.order);
+        alert("Pet was added");
+    }
 
     return (
         <div className="pet">
@@ -17,7 +36,7 @@ export const PetItem = (props) => {
             <p className="pet-breed"> {breed} </p>
             <p className="pet-age"> {age} years old </p>
 
-            <Button text="Request Pet" type="primary" isDisabled={false} action={() => alert("Requested pet")} />
+            <Button text="Request Pet" type="primary" isDisabled={false} action={addPetToCart} />
         
         </div>
     )
--- src/components/pages/ShoppingCartPage/index.jsx
+++ src/components/pages/ShoppingCartPage/index.jsx
@@ -1,9 +1,34 @@
+import { useState, useContext, useEffect } from "react";
 import "./styles.css";
+import PetsOrderContext from "../../../context/petsOrderContext";
+import { OrderItem } from "../../OrderItem";
+import { Button } from "../../Button"
 
 export const ShoppingCartPage = () => {
+
+  const [order, setOrder] = useState([]);
+
+  const globalState = useContext(PetsOrderContext);
+
+  useEffect (
+    () => {
+      setOrder(globalState.order);
+    }, [globalState]
+  )
+
   return (
     <div className="pets-page">
-      My Shopping Cart
+      <h1 className="pets-title"> My Shopping Cart </h1>
+      <div className="order">
+        {
+          order.map((item) => <OrderItem image={item.image} id={item.id} name ={item.name} />)
+        }
+        {
+          order.length === 0 && <p>Nothing in your order yet..</p>
+        }
+      </div>
+        <Button text="Checkout" type="primary"/>
     </div>
   );
 };
+
